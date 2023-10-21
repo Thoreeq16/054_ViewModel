7a427fcc328d8c59c5ca43bd846b28b4dcb28a7e
Thoreeq16 <thoriqhidayatullah16@gmail.com>
2023-10-21 15:43:22 +0700

    Initial commit

---
+++ app/src/main/java/com/example/pertemuan5/data/datasource.kt
@@ -0,0 +1,11 @@
+package com.example.pertemuan5.data
+
+import com.example.pertemuan5.R
+
+object datasource {
+    val jenis = listOf(
+        R.string.laki,
+        R.string.wanita,
+        R.string.wanita2,
+    )
+}
